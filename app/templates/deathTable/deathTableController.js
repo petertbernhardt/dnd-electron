267e225caf2d96941f8b35a052e0c9008a1f901a
Peter Thomas Bernhardt <petertbernhardt@gmail.com>
2018-01-13 10:38:31 -0800

    Created the deathtable json data.

---
+++ app/templates/deathTable/deathTableController.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+    angular.module('dndElectron')
+
+    .controller('DeathTableController',
+        function DeathTableController($scope) {
+
+        }
+    );
+})();
